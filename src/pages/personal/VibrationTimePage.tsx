0d34a8d19ab29c13e687dd4baa6f3f8263b3d616
DOC-weed <aleesfeo1234@gmail.com>
2023-12-11 08:20:11 -0800

    wip-quarter-permonth-component

--- src/pages/personal/VibrationTimePage.tsx
+++ src/pages/personal/VibrationTimePage.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 import NoConsultantSelected from '@/components/NoConsultantSelected';
 import SectionTitle from '@/components/SectionTitle';
 import ActiveEnergy from '@/components/personal/vibrationTime/ActiveEnergy';
+import QuarterPerMonth from '@/components/personal/vibrationTime/QuarterPerMonth';
 import useConsult from '@/hooks/useConsult';
 
 function VibrationTimePage() {
@@ -20,8 +21,10 @@ function VibrationTimePage() {
           </div>
         </div>
         <div className="col-span-4">
-          <SectionTitle title={t('vibrationTime.energy.energy')} color="bg-green-s" />
-          <div className="section-wrap px-2 py-7" />
+          <SectionTitle title={t('vibrationTime.quarterMonth.quarterMonth')} color="bg-green-s" />
+          <div className="section-wrap px-2 py-7">
+            <QuarterPerMonth />
+          </div>
         </div>
       </div>
     </div>
